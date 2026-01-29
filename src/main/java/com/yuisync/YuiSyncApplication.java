9442b9193b39fe4ba382b04089ff2363b73aac7d
NevesRuan <neves.ruan23@gmail.com>
2026-01-28 21:52:57 -0300

    Initial commit

---
+++ src/main/java/com/yuisync/YuiSyncApplication.java
@@ -0,0 +1,13 @@
+package com.yuisync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuiSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuiSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuisync/YuiSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuisync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuiSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
